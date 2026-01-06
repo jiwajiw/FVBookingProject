d06eab168b019b6342e375f5b1469ca253f51a1f
jiwajiw <fjijwjij@gmail.com>
2026-01-06 21:42:33 +0500

    Jenkins check

--- tests/test_health_check.py
+++ tests/test_health_check.py
@@ -48,3 +48,5 @@ def test_ping_timeout(api_client, mocker):
     mocker.patch.object(api_client, 'ping', side_effect=requests.Timeout())
     with pytest.raises(requests.Timeout):
         api_client.ping()
+
+#Jenkins check
